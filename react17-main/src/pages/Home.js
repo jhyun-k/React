3fbce9166f435787875fa1fe89ff3e3d076868f6
jhyun-k <jhkim1238@naver.com>
2022-07-19 11:56:25 +0900

    youtube clone

---
+++ react17-main/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import VideoList from './../components/VideoList';
+import SideMenu from '../components/SideMenu';
+const Home = () => {
+    return (
+        <>
+          <SideMenu  /> 
+          <section className='main-content'>
+             <VideoList />
+          </section>
+        </>
+    );
+};
+
+export default Home;
---
+++ react17-main/src/store/video/videoSlice.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { createSlice ,createAsyncThunk } from '@reduxjs/toolkit'
+
+export const getVideoList = createAsyncThunk(
+    "GET_VIDEO_LIST",
+    async (url) => {
+        try{
+            const res = await axios.get(url)
+            return res.data.items
+        }catch(err){
+            console.log(err)
+        }
+    }
+)
+
+
+const videoSlice =  createSlice({
+    name:'video',
+    initialState:{
+        data:[],
+    },
+    reducers: {  
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(getVideoList.fulfilled,(state,action)=>{
+            console.log('액션페이로드',action.payload)
+            state.data=action.payload;
+        })
+    }
+})
+
+export default videoSlice.reducer
