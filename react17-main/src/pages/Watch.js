f335149b0b85ee7b159d5995d1785b4cfe8c712e
jhyun-k <jhkim1238@naver.com>
2022-07-22 11:46:05 +0900

    publish date loading bar

--- react17-main/src/pages/Watch.js
+++ react17-main/src/pages/Watch.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import VideoView from './../components/VideoView';
-import {useSearchParams} from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import VideoItem from './../components/VideoItem';
-
+import { useSearchParams } from "react-router-dom";
+import {useSelector} from "react-redux";
+import VideoItem from '../components/VideoItem';
 const Watch = () => {
     const [query,setQuery]  = useSearchParams();
     const id=query.get('id')
-    const videoData = useSelector((state)=> state.video.data);
-    const lte10videoData = videoData.filter((item,idx)=>idx<10)
+    const {data} = useSelector((state) => state.video);
+    const lt10VideoData = data.filter((item,idx)=>idx<10)
     return (
         <section className='list content'>
             <VideoView id={id} />
-            <ul>
-            {
-            lte10videoData.map((item,idx)=>(
-            <VideoItem id={id} key={item.snippet.thumbnails.default.url} item={item.snippet} value={item} />))}
+            <ul className='watchList VideoRowList'>
+            { 
+                lt10VideoData.map((item,idx)=>(
+                    <VideoItem 
+                    key={item.snippet.thumbnails.default.url} 
+                    item={item.snippet} value={item}
+                    />
+                ))
+             }
             </ul>
         </section>
     );
